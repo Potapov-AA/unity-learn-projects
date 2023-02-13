a5f2996308e273950c3fded6cbd43f5b3ae07ab8
JESTER <jestterr@mail.ru>
2023-02-13 14:56:10 +0300

    Небольшая демо по сборке игры

---
+++ DemoBuildGame/Assets/Editor/TestPostBuild.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public static class TestPostBuild
+{
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject){
+        Debug.Log("build loaction: " + pathToBuiltProject);
+    }
+}
---
+++ DemoBuildGame/Assets/WebTestObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class WebTestObject : MonoBehaviour
+{
+    private string _message;
+
+    [DllImport("__Internal")]
+    private static extern void ShowAlert(string msg);
+
+    void Start(){
+        _message = "No message yet";
+    }
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)){
+            ShowAlert("Hello out there!");
+        }
+    }
+
+    void OnGUI(){
+        GUI.Label(new Rect(10,10,200,20), _message);
+    }
+
+    public void RespondToBrowser(string message){
+        _message = message;
+    }
+}
