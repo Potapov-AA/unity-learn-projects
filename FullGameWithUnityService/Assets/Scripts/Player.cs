0bf5355fee90bd74b1809948ff87bb997114595a
JESTER <jestterr@mail.ru>
2023-03-14 16:07:48 +0300

    Добавлена интерактивность и определение предметов
    Добавлен уровень для объекта-фурнитура,
    Добавлен RayCast для определения объектов только на этом уровне
    При приближении к объекту вызывается метод для взаимодействия

--- FullGameWithUnityService/Assets/Scripts/Player.cs
+++ FullGameWithUnityService/Assets/Scripts/Player.cs
@@ -7,11 +7,43 @@ public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
 
 
     private bool isWalking;
+    private Vector3 lastInteractDirection;
 
     private void Update() {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+
+    private void HandleInteractions() {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if(moveDirection != Vector3.zero){
+            lastInteractDirection = moveDirection;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, countersLayerMask)){
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                // Объект имеет ClearCounter
+                clearCounter.Interact();
+            }
+            Debug.Log(raycastHit.transform);
+        } else {
+            Debug.Log("-");
+        }
+    }
+
+    private void HandleMovement() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -21,13 +53,13 @@ public class Player : MonoBehaviour {
         float playerHeight = 2f;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
         
-        if (!canMove){
+        if (!canMove) {
             // обработка движения только по оси X
 
             Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
             canMove =!Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
 
-            if (canMove){
+            if (canMove) {
                 moveDirection = moveDirectionX;
             } else {
                 // обработка движения только по оси Y
@@ -35,7 +67,7 @@ public class Player : MonoBehaviour {
                 Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
                 canMove =!Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
 
-                if (canMove){
+                if (canMove) {
                     moveDirection = moveDirectionZ;
                 } else {
                     // Обработка если не может двигаться не по одной из осей
@@ -51,9 +83,5 @@ public class Player : MonoBehaviour {
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
-    }
-
-    public bool IsWalking(){
-        return isWalking;
     }
 }
