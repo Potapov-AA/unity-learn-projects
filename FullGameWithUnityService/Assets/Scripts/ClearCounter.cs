2623add48b740b5c7d2b957df1a5065f313949ee
JESTER <jestterr@mail.ru>
2023-03-17 19:43:11 +0300

    Добавлен функционал для определения родителя
    Теперь KitchenObject могут определять своего родителя и менять его, при этом если меняется родитель объекта, то прошлый родитель обнуляет поле KitchenObject

--- FullGameWithUnityService/Assets/Scripts/ClearCounter.cs
+++ FullGameWithUnityService/Assets/Scripts/ClearCounter.cs
@@ -7,12 +7,46 @@ public class ClearCounter : MonoBehaviour {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing = true;
+
+    private KitchenObject kitchenObject;
+
+
+    private void Update() {
+        if(testing && Input.GetKeyDown(KeyCode.T)) {
+            if(kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
 
     public void Interact() {
-        Debug.Log("Inteact!");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint.transform);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if(kitchenObject == null) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint.transform);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        } else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
     }
 }
