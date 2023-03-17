e69a14433a26597be43a776de09206409f9cb0a7
JESTER <jestterr@mail.ru>
2023-03-17 19:01:59 +0300

    Добавлены ScriptableObects
    Добавлены SO для объектов кухни

--- FullGameWithUnityService/Assets/Scripts/ClearCounter.cs
+++ FullGameWithUnityService/Assets/Scripts/ClearCounter.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class ClearCounter : MonoBehaviour {
 
 
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact() {
         Debug.Log("Inteact!");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint.transform);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }
