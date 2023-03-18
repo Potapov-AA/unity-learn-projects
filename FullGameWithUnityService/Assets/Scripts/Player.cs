8b800ecf17bebbc38fbb9bf0f062499ab8a56eeb
JESTER <jestterr@mail.ru>
2023-03-18 12:54:09 +0300

    Мини фикс в движении игрока
    До этого если игрок бежал вдоль объектов и если пытался посмотреть в сторону объекта вдоль которого бежит, персонаж не поворачивал, теперь он немного поворачивает в данную сторону

--- FullGameWithUnityService/Assets/Scripts/Player.cs
+++ FullGameWithUnityService/Assets/Scripts/Player.cs
@@ -109,7 +109,7 @@ public class Player : MonoBehaviour, IKitchenObjectParents {
             // обработка движения только по оси X
 
             Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
-            canMove =!Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+            canMove = moveDirection.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
 
             if (canMove) {
                 moveDirection = moveDirectionX;
@@ -117,7 +117,7 @@ public class Player : MonoBehaviour, IKitchenObjectParents {
                 // обработка движения только по оси Y
 
                 Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
-                canMove =!Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+                canMove = moveDirection.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
 
                 if (canMove) {
                     moveDirection = moveDirectionZ;
