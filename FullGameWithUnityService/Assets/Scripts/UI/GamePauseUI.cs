1344e5e60ad50d8302d78eddd36d4c6f852934d3
JESTER <jestterr@mail.ru>
2023-04-03 12:07:36 +0300

    Добавлено меню настройки и возможность менять громкость

--- FullGameWithUnityService/Assets/Scripts/UI/GamePauseUI.cs
+++ FullGameWithUnityService/Assets/Scripts/UI/GamePauseUI.cs
@@ -6,16 +6,25 @@ using UnityEngine.UI;
 public class GamePauseUI : MonoBehaviour {
 
 
+    public static GamePauseUI Instance { get; private set; }
+
     [SerializeField] private Button mainMenuButton;
     [SerializeField] private Button resumeButton;
+    [SerializeField] private Button optionsButton;
+
 
 
     private void Awake() {
+        Instance = this;
+
         resumeButton.onClick.AddListener(() => {
             GameManager.Instance.TogglePauseGame();
         });
         mainMenuButton.onClick.AddListener(() => {
             Loader.Load(Loader.Scene.MainMenuScene);
+        });
+        optionsButton.onClick.AddListener(() => {
+            OptionsUI.Instance.Show();
         });
     }
 
